576d9e15b87484ec369fff21315f4ada4f731743
ahsanhabib91 <habibibnakther@gmail.com>
2019-02-12 17:56:19 +0600

    git template added

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('<h1>This is Blog home page</h2>')
+    return render(request, 'blog/home.html')
 
 
 def about(request):
