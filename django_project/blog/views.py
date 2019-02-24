b99c744ea255eae2582fb087e7d2090274000e4f
ahsanhabib91 <habibibnakther@gmail.com>
2019-02-24 17:26:19 +0600

    Lecture-10; Done Upto Update POST

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from .models import Post
 
 
@@ -26,6 +26,16 @@ class PostDetailView(DetailView):
 
 
 class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+    # success_url = reverse_lazy('blog-home') # Default redirect to <model_object>.get_absolute_url()
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
     model = Post
     fields = ['title', 'content']
     # success_url = reverse_lazy('blog-home') # Default redirect to <model_object>.get_absolute_url()
