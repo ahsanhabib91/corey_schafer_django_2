561001d08ab108517e0cd4615a86bcaafb49f7e7
ahsanhabib91 <habibibnakther@gmail.com>
2019-02-24 17:05:14 +0600

    Lecture-10: Done Upto Create POST

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class Post(models.Model):
@@ -12,3 +13,8 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    # calculate the canonical/offical/main URL for an object
+    def get_absolute_url(self):
+        # reverse returns the full path/url as string
+        return reverse('post-detail', kwargs={'pk': self.pk})
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
 
 
@@ -21,6 +23,16 @@ class PostViewList(ListView):
 
 class PostDetailView(DetailView):
     model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+    # success_url = reverse_lazy('blog-home') # Default redirect to <model_object>.get_absolute_url()
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 
 def about(request):
